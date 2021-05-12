06f2d0dbeac5dd3ea45258b956d72e5a8ed20e5a
Anak7n <buzzzzx233@gmail.com>
2021-05-12 14:39:15 +0800

    feat: add routes: projects and project

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,44 +2,60 @@ import { useAuth } from "./context/auth-context";
 import { ProjectListScreen } from "./screens/project-list";
 import styled from "@emotion/styled";
 import { Row } from "./components/lib";
+import { ProjectScreen } from "./screens/project";
 import { ReactComponent as SoftwareLogo } from "./assets/software-logo.svg";
 import { Button, Dropdown, Menu } from "antd";
+import { Route, Routes } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export const AuthenticatedApp = () => {
-  const { user, logout } = useAuth();
-
   return (
     <Container>
-      <Header between={true}>
-        <HeaderLeft gap={true}>
-          <SoftwareLogo width={"18rem"} color={"rgb(38, 132, 255)"} />
-          <h2>用户</h2>
-          <h2>项目</h2>
-        </HeaderLeft>
-        <HeaderRight>
-          <Dropdown
-            overlay={
-              <Menu>
-                <Menu.Item key={"logout"}>
-                  {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                  <Button type={"link"} onClick={logout}>
-                    登出
-                  </Button>
-                </Menu.Item>
-              </Menu>
-            }
-          >
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-            <Button type={"link"} onClick={(event) => event.preventDefault()}>
-              Hi, {user?.name}
-            </Button>
-          </Dropdown>
-        </HeaderRight>
-      </Header>
+      <PageHeader />
       <Main>
-        <ProjectListScreen />
+        <Router>
+          <Routes>
+            <Route path={"/projects"} element={<ProjectListScreen />} />
+            <Route
+              path={"/projects/:projectId/*"}
+              element={<ProjectScreen />}
+            />
+          </Routes>
+        </Router>
       </Main>
     </Container>
+  );
+};
+
+const PageHeader = () => {
+  const { user, logout } = useAuth();
+  return (
+    <Header between={true}>
+      <HeaderLeft gap={true}>
+        <SoftwareLogo width={"18rem"} color={"rgb(38, 132, 255)"} />
+        <h2>用户</h2>
+        <h2>项目</h2>
+      </HeaderLeft>
+      <HeaderRight>
+        <Dropdown
+          overlay={
+            <Menu>
+              <Menu.Item key={"logout"}>
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <Button type={"link"} onClick={logout}>
+                  登出
+                </Button>
+              </Menu.Item>
+            </Menu>
+          }
+        >
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <Button type={"link"} onClick={(event) => event.preventDefault()}>
+            Hi, {user?.name}
+          </Button>
+        </Dropdown>
+      </HeaderRight>
+    </Header>
   );
 };
 
--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,7 +1,9 @@
 import { User } from "./search-panel";
 import { Table, TableProps } from "antd";
 import dayjs from "dayjs";
+import { Link } from "react-router-dom";
 
+// TODO 把所有 id 都转换为 number
 export interface Project {
   id: string;
   name: string;
@@ -23,8 +25,10 @@ export const List = ({ users, ...props }: ListProps) => {
       columns={[
         {
           title: "名称",
-          dataIndex: "name",
           sorter: (a, b) => a.name.localeCompare(b.name),
+          render(value, project) {
+            return <Link to={String(project.id)}>{project.name}</Link>;
+          },
         },
         {
           title: "项目组",
